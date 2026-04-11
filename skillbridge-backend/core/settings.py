c59ce62138cb79f8007b88d8195c4e602106818e
Azel <villanueva.azel@dnsc.edu.ph>
2026-04-11 23:01:24 +0800

    update env

--- skillbridge-backend/core/settings.py
+++ skillbridge-backend/core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-h0v%7n_)yg_ycj8&3#ya9j&wi=s-c+tk)ggnldve0c6@i-d1++
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
